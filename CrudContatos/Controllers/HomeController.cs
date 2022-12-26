aa034b12b37c06afd20fc0c2094b63ca372df880
Rogerio Gomes <rogerim00@hotmail.com>
2022-12-26 11:56:26 -0300

    Start front-end

--- CrudContatos/Controllers/HomeController.cs
+++ CrudContatos/Controllers/HomeController.cs
@@ -5,14 +5,7 @@ using System.Diagnostics;
 namespace CrudContatos.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+    { 
         public IActionResult Index()
         {
             return View();
