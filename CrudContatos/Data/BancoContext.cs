5386f0dfc5922324bf86843211827e1a1c976e5c
Rogerio Gomes <rogerim00@hotmail.com>
2022-12-27 15:57:34 -0300

    Entity framework core package tables and connections

---
+++ CrudContatos/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudContatos.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
